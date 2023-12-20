7b094c1e78eafdac77d0a7b34e79dd1977a6f6a7
MariaBetancur <mabo6241@gmail.com>
2023-12-20 16:00:57 -0500

    v1

--- service/index.js
+++ service/index.js
@@ -2,9 +2,11 @@
 const createUSer = require('./auth');
 const loginUser = require('./auth');
 const products = require('./products');
+const days = require('./days');
 
 module.exports = {
   createUSer,
   products,
   loginUser,
+  days,
 };
