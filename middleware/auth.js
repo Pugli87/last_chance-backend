0a49ddbcecc375cee240ee36c77030722119214b
miguelangelacosta <113574101+miguelangelacosta@users.noreply.github.com>
2023-12-21 00:08:08 -0500

    correcion

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-const { Unauthorized } = require('http-errors');
+const {Unauthorized } = require('http-errors');
 const jwt = require('jsonwebtoken');
 
 const { User } = require('../models');
@@ -10,12 +10,12 @@ const auth = async (req, res, next) => {
 	const [bearer, token] = authorization.split(" ");
 	try {
 		if (bearer !== "Bearer") {
-		// throw new Unauthorized ("Not authorized");
+		 throw new Unauthorized ("Not authorized");
 		}
 		const { id } = jwt.verify(token, SECRET_KEY);
 		const user = await User.findById(id);
 		if (!user || !user.token) {
-			// throw new Unauthorized ("Not authorized");
+			 throw new Unauthorized ("Not authorized");
 		}
 		req.user = user;
 		next();
