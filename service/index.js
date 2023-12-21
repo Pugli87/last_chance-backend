58e61301b1603106f1a57c7c5ff64ba9b38c7fdf
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-12-20 20:13:25 -0500

    categories ok

--- service/index.js
+++ service/index.js
@@ -1,11 +1,8 @@
 // controllers/index.js
 const auth = require('./auth');
 const products = require('./products');
-const logout = require('./auth');
 
 module.exports = {
   auth,
   products,
-  loginUser,
-  logout,
 };
